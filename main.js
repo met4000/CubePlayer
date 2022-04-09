b26e1f0f07611e83ad469802da6db70ea6864d4a
met4000 <nathsharwor@hotmail.com>
2022-04-09 16:46:32 +0800

    cube logic (no modification)

--- main.js
+++ main.js
@@ -15,3 +15,49 @@ Object.values(sliders).forEach(v => v.addEventListener("input", () => {
                        + `rotateZ(${sliders.z.value}deg)`;
 }));
 
+
+// cube manipulation
+
+class Move {
+  constructor(normalAxis, offset, direction) {
+    this.normal = normalAxis;
+    this.offset = offset;
+    this.direction = direction;
+  }
+
+  reversed() {
+    return new Move(this.normal, this.offset, -this.direction);
+  }
+}
+
+let enact = move => {
+  // ! TODO
+};
+
+let defaultNotation = {
+  R: [new Move("x", 1, -1)],
+  L: [new Move("x", -1, 1)],
+  U: [new Move("y", 1, -1)],
+  D: [new Move("y", -1, 1)],
+  F: [new Move("z", 1, -1)],
+  B: [new Move("z", -1, 1)],
+};
+
+let perform = (str, notation = defaultNotation) => {
+  for (let i = 0; i < str.length; i++) {
+    let moves = notation[str[i]];
+    if (moves === undefined) throw new Error(`invalid move "${str[i]}"`);
+
+    if (str[i+1] === "'") { // i.e. reversed
+      moves = moves.map(move => move.reversed());
+      i++;
+    }
+    
+    if (/\d/.test(str[i+1])) { // i.e. repeated
+      moves = Array.from({length: str[i+1]}, () => moves).reduce((r, v) => r.concat(v));
+      i++;
+    }
+
+    moves.forEach(move => enact(move));
+  }
+};
