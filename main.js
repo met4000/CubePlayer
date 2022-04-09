a6719b9cf808f7e51bc13adfd72b38902b89c481
met4000 <nathsharwor@hotmail.com>
2022-04-09 20:13:45 +0800

    calculate perpendincular cell changes

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ class AxisVector {
     this.axis = axis;
     this.magnitude = magnitude;
   }
+
+  mapAxis(f = (a, m) => a) { return new AxisVector(f(this.axis, this.magnitude), this.magnitude); }
+  mapMag(f = (m, a) => m) { return new AxisVector(this.axis, f(this.magnitude, this.axis)); }
+
+  equals(v) { return this.axis === v.axis && this.magnitude === v.magnitude; }
 }
 let crossProduct = (v1, v2) => { // ! for AxisVectors only
   const ring = [X_AXIS, Y_AXIS, Z_AXIS];
@@ -37,7 +42,7 @@ let crossProduct = (v1, v2) => { // ! for AxisVectors only
 class Move {
   constructor(normalAxis, offset, rotation) {
     this.normal = new AxisVector(normalAxis, offset);
-    this.rotation = rotation;
+    this.rotation = rotation; // must be 1 or -1
   }
 
   reversed() {
@@ -45,42 +50,74 @@ class Move {
   }
 }
 
-let faceInfoArr = [
-  { face: "front",  normal: new AxisVector(Z_AXIS,  1) },
-  { face: "back",   normal: new AxisVector(Z_AXIS, -1) },
-  { face: "right",  normal: new AxisVector(X_AXIS,  1) },
-  { face: "left",   normal: new AxisVector(X_AXIS, -1) },
-  { face: "top",    normal: new AxisVector(Y_AXIS,  1) },
-  { face: "down",   normal: new AxisVector(Y_AXIS, -1) },
+let faceArr = [
+  { name: "front",  normal: new AxisVector(Z_AXIS,  1) },
+  { name: "back",   normal: new AxisVector(Z_AXIS, -1) },
+  { name: "right",  normal: new AxisVector(X_AXIS,  1) },
+  { name: "left",   normal: new AxisVector(X_AXIS, -1) },
+  { name: "top",    normal: new AxisVector(Y_AXIS,  1) },
+  { name: "down",   normal: new AxisVector(Y_AXIS, -1) },
 ];
 
 const defaultRing = [0, 1, 3, 2];
 // todo: exchange elements instead of exchanging classes (allows for e.g. pictures/text)
 let enact = move => {
-  for (let faceInfo of faceInfoArr) {
-    if (faceInfo.normal.axis === move.normal.axis) { // parallel plane
+  let newCells = {}; // e.g. { front: [blue, red] }
+
+  for (let face of faceArr) {
+    if (face.normal.axis === move.normal.axis) { // parallel plane
       // continue if planes are not equal
-      if (move.normal.magnitude * faceInfo.normal.magnitude != maxCubeOffset) continue;
+      if (move.normal.magnitude * face.normal.magnitude != maxCubeOffset) continue;
 
       // perform a walk of each "ring" on the face, exchanging elements
       // todo generalise for multiple rings (2x2 has only one ring)
 
-      let faceElements = document.getElementById(faceInfo.face).children;
+      let faceElements = document.getElementById(face.name).children;
 
       let ring = [...defaultRing];
-      if (faceInfo.normal.magnitude * move.rotation < 0) ring.reverse();
+      if (face.normal.magnitude * move.rotation < 0) ring.reverse();
 
       let temp = faceElements[ring[0]].className;
       for (let i = 1; i < ring.length; i++)
         faceElements[ring[i - 1]].className = faceElements[ring[i]].className;
       faceElements[ring[ring.length - 1]].className = temp;
     } else { // intersecting plane
-      // TODO
+      let faceElements = document.getElementById(face.name).children;
+
+      let op = (a, b) => undefined;
+      if (crossProduct(face.normal, move.normal.mapMag(m => Math.abs(m))).magnitude < 0) {
+        op = (a, b) => a % b;
+      } else {
+        op = (a, b) => Math.floor(a / b);
+      }
+
+      let fmProduct = crossProduct(face.normal, move.normal);
+
+      // "invert" around size (e.g. size = 4 => 3 becomes 0, 2 becomes 1) if fmProduct > 0
+      let appliedOp = (i, c) => op(i, cubeSize) == c(maxCubeOffset - move.normal.magnitude);
+      let check = i => false;
+      if (fmProduct.magnitude > 0) {
+        check = i => appliedOp(i, n => cubeSize - 1 - n);
+      } else {
+        check = i => appliedOp(i, n => n);
+      }
+
+      let nextFace = fmProduct.mapMag(m => m * -move.rotation);
+
+      // guaranteed to be in order, but may need to be reversed
+      let movingElements = [...faceElements].filter((_, i) => check(i));
+      if (nextFace.magnitude > 0) movingElements.reverse(); // ... reverse if needed
+
+      newCells[faceArr.find(v => v.normal.equals(nextFace)).name] = movingElements;
     }
+  }
+
+  for (let [name, elements] of Object.entries(newCells)) {
+    // todo
   }
 };
 
-let defaultNotation = {
+let defaultNotation = { // ! only for a 2x2 (or 3x3)
   R: [new Move(X_AXIS,  1, -1)],
   L: [new Move(X_AXIS, -1,  1)],
   U: [new Move(Y_AXIS,  1, -1)],
