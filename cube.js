2bda40e9cc3608d3721f3139cb45e0c8a7fe9f10
met4000 <nathsharwor@hotmail.com>
2022-04-10 12:07:14 +0800

    add help func

--- cube.js
+++ cube.js
@@ -176,3 +176,33 @@ let rotate = (str, options = undefined) => perform(str, { ...options, notationSe
   F: [new Move(Z_AXIS,  1, -1), new Move(Z_AXIS, -1,  1)],
   B: [new Move(Z_AXIS,  1,  1), new Move(Z_AXIS, -1, -1)],
 }});
+
+let helpObj = {
+  help: "help(command: string = undefined) // " +
+        "Displays this help message. If `command` is specified, only " +
+        "displays the help message for the specified command, or an error message if it does not exist.",
+  perform: "perform(str: string, options: Object = undefined) // " +
+           "performs the sequence `str` on the cube. `options` is an object containing " +
+           "`\"notationSet\"` (defaults to Singmaster notation) and/or `\"delay\"` (defaults to 0).",
+  rotate: "rotate(str: string, options: Object = undefined) // " +
+          "performs the rotation sequence `str` on the cube. `rotate(...)` is an alias for `perform(...)` " +
+          "under the hood, with the default `\"notationSet\"` changed to one that rotates the cube " +
+          "towards the face specified. `\"F\"` and `\"B\"` rotate the cube clockwise, " +
+          "respective to the face specified.",
+  toggleButtons: "toggleButtons() // " +
+                 "toggles the on-cube move buttons on/off. May cause problems when active.",
+};
+let help = command => {
+  if (command !== undefined) { // display specific command help
+    if (helpObj[command] === undefined) {
+      console.log("Command does not exist. Run `help()` for a list of all commands.");
+      return false;
+    }
+
+    console.log(helpObj[command]);
+  } else { // display all command help
+    Object.values(helpObj).forEach(v => console.log(v));
+  }
+
+  return true;
+};
